e67c42504f0d03ed389c5fb9403d8871940c347b
ajsnyde <ajsnyde@bgsu.edu>
2016-02-06 18:00:47 -0500

    Implemented some basic JSoup
    Obviously, the scope of this project has changed. Rather than create the
    application from the ground up, it is simpler and more productive to
    create a streamlined form the the online version. I've moved to JSoup
    (keeping the old code for awhile) for processing the pages. Noted bugs:
    - The alert buttons are not removed using conventional JSoup functions.
    Probably added through javascript, and will have to block that somehow
    - The late stages of the hexagon puzzles don't render properly. This is
    on my phone (1920x1080), and could have a number of causes (CSS
    attributes, webview settings, etc)
    
    At this point of the project, my main goals are to:
    - Finish sanitizing puzzle pages
    - Add activity pages for puzzle selection
    - Finalization + installation (I've never considered putting something
    on the Google Play store, and this may or may not materialize)

--- app/src/main/java/com/asaddisonsnyder/webview/BrowserTest.java
+++ app/src/main/java/com/asaddisonsnyder/webview/BrowserTest.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -34,10 +38,22 @@ public class BrowserTest extends AppCompatActivity {
         /** The system calls this to perform work in a worker thread and
          * delivers it the parameters given to AsyncTask.execute() */
         protected String doInBackground(String... urls) {
+
+
+
+
+            Document doc = null;
             String html = "";
             try {
-                URL oracle = new URL(urls[0]);
-                URLConnection yc = oracle.openConnection();
+
+                URL url = new URL(urls[0]);
+                doc  = Jsoup.connect(url.toString()).get();
+                doc.select("footer").remove();
+                doc.getElementsByAttributeValue("class", "navbar navbar-inverse navbar-fixed-top").remove();
+                //doc.getElementsByAttribute("role").remove();
+                doc.select("body").attr("style", "padding-top:0px");
+
+                URLConnection yc = url.openConnection();
                 BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
 
                 String inputLine;
@@ -50,7 +66,7 @@ public class BrowserTest extends AppCompatActivity {
 
             html = html.replaceAll("<footer\\b[^>]*>(.*?)</footer>", "");
 
-            return html;
+            return doc.toString();
         }
         protected void onPostExecute(String result) {
             webview.loadDataWithBaseURL("https://regexcrossword.com/challenges/hexagonal/puzzles/1", result, "text/html", "utf-8", "");
